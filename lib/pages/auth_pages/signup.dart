897a82bbb8c6ec1b104596daefb3419fd0d407ce
Daniel Adeleke <danieladeleke05@gmail.com>
2025-04-03 22:19:14 +0100

    Did some more styling to the page

--- lib/pages/auth_pages/signup.dart
+++ lib/pages/auth_pages/signup.dart
@@ -10,14 +10,45 @@ class Signup extends StatelessWidget {
         backgroundColor: Colors.white,
         body: ListView(
           children: [
-            Container(
-              color: Colors.grey[100],
-              child: Icon(
-                Icons.translate_sharp,
-                size: 85,
-                color: Colors.blue[900],
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Container(
+                height: 150,
+                color: Colors.grey[200],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.translate_sharp,
+                    size: 85,
+                    color: Colors.blue[900],
+                  ),
+                ),
               ),
             ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red[300]
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      alignment: Alignment.topLeft,
+                      child: const Text(
+                        "Create Account",
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       )
