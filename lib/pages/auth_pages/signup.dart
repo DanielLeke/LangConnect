1e03142ff3d06ed8a48cc0e4eb11d1b8ef0a16f8
Daniel Adeleke <danieladeleke05@gmail.com>
2025-04-04 12:53:53 +0100

    Did some refactoring to make the code more readable

--- lib/pages/auth_pages/signup.dart
+++ lib/pages/auth_pages/signup.dart
@@ -14,25 +14,7 @@ class Signup extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               direction: Axis.vertical,
               children: [
-                Container(
-                  height: 150,
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(12),
-                      bottomRight: Radius.circular(12)
-                    )
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(
-                      Icons.translate_sharp,
-                      size: 85,
-                      color: Colors.blue[900],
-                    ),
-                  ),
-                ),
+                const TranslateIcon(),
                 Flexible(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -40,31 +22,10 @@ class Signup extends StatelessWidget {
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(8),
                       ),
-                      child: Column(
+                      child: const Column(
                         children: [
-                          Container(
-                            alignment: Alignment.topLeft,
-                            child: const Text(
-                              "Create Account",
-                              style: TextStyle(
-                                fontSize: 35,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black
-                              ),
-                            ),
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                              label: Text("Email"),
-                              enabled: true,
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(8)),
-                                borderSide: BorderSide(color: Colors.blue[900]!, width: 2)
-                              ),
-                    
-                            ),
-                            obscureText: false,
-                          ),
+                          SignupText(),
+                          EmailField(),
                         ],
                       ),
                     ),
@@ -75,6 +36,77 @@ class Signup extends StatelessWidget {
           ],
         ),
       )
+    );
+  }
+}
+
+class EmailField extends StatelessWidget {
+  const EmailField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        label: Text("Email"),
+        enabled: true,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          borderSide: BorderSide(color: Colors.blue[900]!, width: 2)
+        ),
+      ),
+      obscureText: false,
+    );
+  }
+}
+
+class SignupText extends StatelessWidget {
+  const SignupText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topLeft,
+      child: const Text(
+        "Create Account",
+        style: TextStyle(
+          fontSize: 35,
+          fontWeight: FontWeight.bold,
+          color: Colors.black
+        ),
+      ),
+    );
+  }
+}
+
+class TranslateIcon extends StatelessWidget {
+  const TranslateIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(12),
+          bottomRight: Radius.circular(12)
+        )
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(
+          Icons.translate_sharp,
+          size: 85,
+          color: Colors.blue[900],
+        ),
+      ),
     );
   }
 }
