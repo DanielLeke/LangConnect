d8ca41318df462cbc8dc72c06a469b65d07c059f
Daniel Adeleke <danieladeleke05@gmail.com>
2025-04-05 14:01:05 +0100

    Built an app bar

--- lib/pages/translation_pages/text_translation.dart
+++ lib/pages/translation_pages/text_translation.dart
@@ -7,7 +7,30 @@ class TextTranslation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[200],
+      appBar: const TranslateAppBar(),
+    );
+  }
+}
+
+class TranslateAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TranslateAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.blue[900],
+      title: const Text(
+        "Language Translator",
+        style: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
